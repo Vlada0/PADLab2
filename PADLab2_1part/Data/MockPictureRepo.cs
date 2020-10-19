eea1d66bbe3e0d1bcd527214281ac0dd1f6803d6
vlada061098@gmail.com <vlada061098@gmail.com>
2020-10-19 21:07:56 +0300

    Wip: Add CRUD operation for Pictures

---
+++ PADLab2_1part/Data/MockPictureRepo.cs
@@ -0,0 +1,45 @@
+﻿using PADLab2_1part.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace PADLab2_1part.Data
+{
+    public class MockPictureRepo : IPictureRepo
+    {
+        static readonly List<Picture> pictures = new List<Picture>();
+        
+        public Picture CreatePicture(Picture picture)
+        {
+            pictures.Add(picture);
+            Console.WriteLine(pictures);
+            return picture;
+        }
+
+        public void Delete(int id)
+        {
+            pictures.RemoveAll(s=>s.Id == id);
+        }
+
+        public Picture GetPictureById(int id)
+        {
+            var picture = pictures.Where(s => s.Id == id);
+            return picture.FirstOrDefault();
+        }
+
+        public IEnumerable<Picture> GetPictures()
+        {
+            return pictures;
+        }
+
+        public Picture Update(Picture picture)
+        {
+            var _picture = pictures.Where(s => s.Id == picture.Id).FirstOrDefault();
+            _picture.Copy(picture);
+            Console.WriteLine(pictures);
+            return _picture;
+        }
+    }
+}
---
+++ PADLab2_1part/Models/Picture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PADLab2_1part.Models
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+
+        public void Copy(Picture picture)
+        {
+            Name = picture.Name;
+            Image = picture.Image;
+            Author = picture.Author;
+            Description = picture.Description;
+        }
+
+    }
+}
